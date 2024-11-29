1556935ec7c0797322d4210f7b5bde4f439ef2be
HEAMIN1011 <mintwo20@nate.com>
2024-11-29 09:40:24 +0900

    배열 동적인 컴포넌트 그리기 퀴즈&연습

--- src/practice/ColorBox.js
+++ src/practice/ColorBox.js
@@ -1,9 +1,12 @@
+import './Practice002.css';
+import { useState } from 'react';
 
 function ColorBox(props){
 
     return (
-        <div className="box" style={{backgroundColor : props.bgColor}}></div>
-    )
+        <div className="box" style={{backgroundColor : props.bgColor}}>
+        </div>
+    );
 }
 
 export default ColorBox;
